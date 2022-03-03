91db3db5d257181178c9a7a2d8794dc5cd236d6b
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-03 11:36:25 +0200

    Implement sending messages to users

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import User
+from .models import User, Message
 
 def index(request):
     return render(request, 'pages/index.html')
@@ -7,7 +7,7 @@ def index(request):
 def register(request):
     if request.method == 'POST':
         body = request.POST
-        user = User(id_generator(), body['username'], body['password'])
+        user = User(id=id_generator(), username=body['username'], password=body['password'])
         user.save()
 
     return render(request, 'pages/register.html')
@@ -24,10 +24,29 @@ def login(request):
 
     return render(request, 'pages/login.html')
 
-def app(request, user):
+def app(request, user=None):
+    if request.method == 'POST':
+        body = request.POST
+        receiver = User.objects.get(username=body['receiver'])
+        print(receiver)
+
+        message = Message(id=id_generator(False), content=body['content'], receiver=receiver)
+        print(message)
+        message.save()
+
+        print(Message.objects.all())
+
+    if not user:
+        return render(request, 'pages/app.html', { 'username': 'Tester', 'users': [ user.username for user in User.objects.all() ] })
+
     return render(request, 'pages/app.html', user)
 
-def id_generator():
-    id = User.objects.latest('id').id
+def id_generator(user=True):
+    if user:
+        id = User.objects.latest('id').id
+    else:
+        id = Message.objects.latest('id').id
+
     id += 1
+
     return id
