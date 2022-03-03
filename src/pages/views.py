7ee859dcd528b7340bbf00d7488339d3088a3231
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-03-03 10:08:10 +0200

    Send login data via post method

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,4 +5,7 @@ def index(request):
     return render(request, 'pages/index.html')
 
 def login(request):
+    if request.method == 'POST':
+        body = request.POST
+
     return render(request, 'pages/login.html')
